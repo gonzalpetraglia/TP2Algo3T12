7653b2e74694b5453defbf91d6ea213db9adc089
gonzalpetraglia <gonzalpetraglia@gmail.com>
2016-05-28 10:02:34 -0300

    Agregue carpetas

---
+++ test/fiuba/algo3/testsUnitarioModelo/PosicionUnitarioTest.java
@@ -0,0 +1,14 @@
+package fiuba.algo3.testsUnitarioModelo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PosicionUnitarioTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
